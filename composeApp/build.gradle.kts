3525dd57a1329cfb5178fb0f9322466cd6d28d3e
dhyllrocoedu-ai <dhyllroco.edu@gmail.com>
2026-07-18 17:57:11 +0800

    v5.0.11: WiX custom uninstall actions, jpackage config, version bump to 5.0.10

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.portalhost"
-version = "5.0.7"
+version = "5.0.10"
 
 kotlin {
     jvm("desktop")
@@ -57,7 +57,7 @@ compose.desktop {
                 org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi,
             )
             packageName = "PortalHost"
-            packageVersion = "5.0.7"
+            packageVersion = "5.0.10"
             description = "Minecraft Java Edition Server Manager"
             vendor = "PortalHost"
             modules("java.sql", "java.naming", "java.management", "java.net.http")
